7bc36c26e8db02f41a133f4ba88248d624d5d865
Phattoko - Phatiski <naphatism@hotmail.com>
2026-04-08 09:01:02 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center">
+  <img style="filter: invert(100%);" src="https://latex.codecogs.com/svg.latex?\Huge{\text{P}_{\!\!\text{H}}\>\!\!\text{A}^{\!\!\text{T}}\!\text{R}_{\;\!\!\text{I}}}\>\!\!\text{S}^{\>\!\!\text{K}}\;\!\!\!\text{Y}">
+</p>
+
+the phatiski home page
+
+[https://phatiski.github.io/](https://phatiski.github.io/)
---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMenu() {
+    const sideBar = document.getElementById('side-bar');
+    const mainArea = document.getElementById('main-area');
+    sideBar.classList.toggle('active');
+    mainArea.classList.toggle('active');
+}
+
+document.addEventListener('click', function(event) {
+    const sideBar = document.getElementById('side-bar');
+    const mainArea = document.getElementById('main-area');
+    const menuToggle = document.querySelector('.menu-toggle');
+    if (!sideBar.contains(event.target) && !menuToggle.contains(event.target)) {
+        sideBar.classList.remove('active');
+        mainArea.classList.remove('active');
+    }
+});
+
+const mediaDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+mediaQuery.addEventListener('change', (event) => {
+  if (event.matches) {
+    document.querySelector("img.header-logo").src = "svg-logo/logo-light.svg"; // โลโก้สำหรับโหมดมืด
+  } else {
+    document.querySelector("img.header-logo").src = "svg-logo/logo-dark.svg"; // โลโก้สำหรับโหมดสว่าง
+  }
+});
